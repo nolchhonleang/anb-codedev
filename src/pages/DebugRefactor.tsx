81b30b7590d02806e2fa477b596e6bfba01b3df8
nolchhonleang <Leang.codes@gmail.com>
2025-11-06 20:14:40 +0700

    Add DebugRefactor component

---
+++ src/pages/DebugRefactor.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DebugRefactor = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Debug & Refactor</h1>
+      <p>This is a placeholder for the Debug & Refactor feature.</p>
+    </div>
+  );
+};
+
+export default DebugRefactor;
